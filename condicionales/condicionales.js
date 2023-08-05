ac072e086b34fac974d469cfc359113264d6e4ff
bellavergara <bellaiivanavergara@gmail.com>
2023-08-05 17:32:01 -0300

    ejercicios completos

--- condicionales/condicionales.js
+++ condicionales/condicionales.js
@@ -1,3 +1,4 @@
+
 //ejercicios condicionales//
 //1Realizar un programa que reciba 2 números y que imprima por consola si el primer número es mayor que el segundo.//
 
@@ -21,14 +22,14 @@ if(numerouno == numeroDos){
 
 //3 Realizar un programa que reciba 2 números y que imprima por consola cuál de los 2 números es el más grande o si son iguales.//
 
-let num1 = Number(prompt("ingresa el primer dato"));
-let num2 = Number(prompt("ingresa el segundo dato"));
+let numeroo1 = Number(prompt("ingresa el primer dato"));
+let numeroo2 = Number(prompt("ingresa el segundo dato"));
 
-numerosAcomparar = num1== num2;
-if(num1>num2) {
-    console.log(`el numero${num1} es mayor que ${num2}`);
-} else if (num2>num1) { 
-    console.log(`el numero ${num2} es mayor que ${num1}`);
+numerosAcomparar = numeroo1== numeroo2;
+if(numeroo1>numeroo2) {
+    console.log(`el numero${numeroo1} es mayor que ${numeroo2}`);
+} else if (numeroo1>numeroDos) { 
+    console.log(`el numero ${numeroo1} es mayor que ${numeroDos}`);
 }else{
     console.log(numerosAcomparar);
 }
@@ -102,12 +103,290 @@ y determine si estás capacitado para conducir, para saber esto la persona deber
 let nombrePersona = prompt("indica tu nombre aqui");
 let edadpersonas = Number(prompt("ingresa tu edad aqui"))
 let alturaPersona = Number(prompt("indica tu altura aqui"));
-let visionPersona = Number(prompt("indica  tu visio aqui"));
+let visionPersona = Number(prompt("indica tu vision aqui"));
 
-if(edadpersonas>= 18){
+if(edadpersonas>= 18 && alturaPersona>150 && visionPersona>8) {
     console.log("estas capacitad@ para conducir");
-}else if (alturaPersona> 150 && alturaPersona>8){
-    console.log(" tienes la altura suficiente para conducir");
+
 }else{
     console.log("no tienes los requisitos necesarios para conducir");
 }
+
+
+/* 7.Realizar un programa que permita el ingreso por pantalla de los siguientes datos:
+● Nombre del cliente.
+● Pase ( vip o normal )
+● Si posee entrada ( si o no ).
+Las personas que posean:
+Tú mismo nombre tienen ingreso libre ( mostrar un mensaje de bienvenida ) ó Pase vip tienen ingreso libre ( mostrar un mensaje de bienvenida )
+Si posee entrada preguntar ¿desea utilizarla?, en caso: afirmativo ( mostrar un mensaje de bienvenida )
+Y por último en caso de:
+No tener el mismo nombre y no tener un pase vip y no tener entrada, preguntar ¿desea comprar? en caso:
+negativo ( mostrar mensaje de despedida )
+afirmativo preguntar dinero disponible, si posee:
+$1000 o mas, ( mostrar un mensaje venta exitosa y de bienvenida ) Menos de $1000 ( mostrar un mensaje de rechazo de venta )*/
+
+let nombreCliente = prompt("ingrese su nombre, por favor");
+let pase = prompt("indica tu pase : VIP o NORMAL");
+let entrada = prompt("posee entrada? : SI |NO");
+
+if(nombreCliente.toLowerCase()=="bella" || pase.toLowerCase()=="vip" ){
+    console.log("Bienvenid@ al lugar");
+} 
+else if(entrada.toLowerCase() == "si"){
+    let respuesta = prompt("desea utilizar la entrada? SI|NO");
+    if(respuesta.toLowerCase() == "si"){
+        console.log("bienvenid@ puedes pasar")
+    }
+    else{
+        console.log("no puedes ingresar sin tu entrada");
+    }
+}
+else{
+    let respuesta = prompt("desea comprar una entrada? SI|NO ");
+    if (respuesta.toLowerCase() == "si"){
+        let  dineroUsuario = Number(prompt("cuanto dinero dispone?"));
+        if(dineroUsuario >= 1000){
+            console.log("la venta ha sido exitosa, bienvenid@");
+        }
+        else{
+            console.log("no se pudo concretar la venta, vuelva a intentarlo otro dia");
+        }
+    }
+    else{
+        console.log("hasta luego");
+    }
+}
+
+/* 8  
+Realizar un juego de adivinanza estableciendo un valor entre 1 y 10 en una variable llamada numeroIncognita y que permita en 3 intentos adivinar el número.
+El usuario deberá ingresar un numero del 1 al 10 por pantalla en 3 intentos el cual se deberá guardar en una variable llamada numeroIngresado,
+y en cada intento deberás mostrarle un mensaje al usuario diciendo: “el numero ingresado es mayor, vuelve a intentarlo” o “el numero ingresado es menor, vuelve a intentarlo” o en caso de adivinar, un mensaje que diga: “Ganaste, has adivinado el número.”
+No te preocupes si usas mucho código repetido, más adelante verás cómo realizar este juego de manera más eficiente.*/
+
+const numeroIncognita = 10;
+// primer intento
+let numeroIngresado = Number(prompt("ingresa un numero del 1 al 10, para adivinar"));
+if(numeroIngresado == numeroIncognita){
+    console.log(" Ganaste, has adivinado el número.");
+}
+else if (numeroIngresado < numeroIncognita){
+    console.log("el numero ingresado es menor, vuelve a intentarlo");
+    
+    // segundo intento
+    numeroIngresado = Number(prompt("ingresa un numero del 1 al 10, para adivinar"));
+    if(numeroIngresado == numeroIncognita){
+        console.log(" Ganaste, has adivinado el número.");
+    }
+    else if(numeroIngresado<numeroIncognita){
+        console.log("el numero ingresado es menor, vuelve a intentarlo");
+        
+        // tercer intento
+        numeroIngresado = Number(prompt("ingresa un numero del 1 al 10, para adivinar"));
+        if(numeroIngresado == numeroIncognita){
+            console.log(" Ganaste, has adivinado el número.");
+        }
+        else{
+            console.log("has utulizado tus 3 intentos, fin del juego");
+        }
+    }
+    else{
+        console.log("el numero ingresado es mayor, vuelve a intentarlo");
+
+        // tercer intento
+        numeroIngresado = Number(prompt("ingresa un numero del 1 al 10, para adivinar"));
+        if(numeroIngresado == numeroIncognita){
+            console.log(" Ganaste, has adivinado el número.");
+        }
+        else{
+            console.log("has utulizado tus 3 intentos, fin del juego");
+        }
+    }
+}
+else{
+    console.log("el numero ingresado es mayor, vuelve a intentarlo");
+    // segundo intento
+    numeroIngresado = Number(prompt("ingresa un numero del 1 al 10, para adivinar"));
+    if(numeroIngresado == numeroIncognita){
+        console.log(" Ganaste, has adivinado el número.");
+    }
+    else if(numeroIngresado<numeroIncognita){
+        console.log("el numero ingresado es menor, vuelve a intentarlo");
+        
+        // tercer intento
+        numeroIngresado = Number(prompt("ingresa un numero del 1 al 10, para adivinar"));
+        if(numeroIngresado == numeroIncognita){
+            console.log(" Ganaste, has adivinado el número.");
+        }
+        else{
+            console.log("has utulizado tus 3 intentos, fin del juego");
+        }
+    }
+    else{
+        console.log("el numero ingresado es mayor, vuelve a intentarlo");
+
+        // tercer intento
+        numeroIngresado = Number(prompt("ingresa un numero del 1 al 10, para adivinar"));
+        if(numeroIngresado == numeroIncognita){
+            console.log(" Ganaste, has adivinado el número.");
+        }
+        else{
+            console.log("has utulizado tus 3 intentos, fin del juego");
+        }
+    }
+}
+
+
+
+/*9
+Crear un programa que permita ingresar tu edad y
+decir si eres un infante (0 a 12 años),
+adolescente(13 a 18 años),
+un mayor joven (19 a 45 años) o un anciano (más de 45 años),
+y en caso de ingresar una edad mayor a 100 mostrar un mensaje preguntando si en realidad tiene esa edad.
+*/
+
+const  edadUsuario = Number(prompt("ingresa tu edad"));
+    if(edadUsuario<=12){
+        console.log("eres infante");
+    }
+    else if(edadUsuario>=100){
+        console.log(prompt("enrealidad tienes esa edad?"));
+    }
+    else if(edadUsuario<=18){
+        console.log("eres un adolescente");
+    }
+    else if(edadUsuario<=45){
+        console.log( "eres un joven mayor");
+    }
+    else if (edadUsuario>45){
+        console.log("eres un anciano");
+    }
+    
+    
+    /*10Crear un programa que permita el ingreso de “PIEDRA”, “PAPEL” o “TIJERAS” a 2 jugadores y muestre en pantalla cuál de los 2 ha ganado o si han empatado. 
+    En caso de algún ingreso incorrecto mostrar por pantalla que uno de los jugadores ha hecho trampa.*/
+
+    // ingreso de "PIEDRA", "PAPEL",o "TIJERA" a dos usuarios//
+    //mostrar en pantalla el ganador del juego // O // si enpataron
+    //en caso de ingreso incorrecto, mostrar por pantalla que uno de los jugadores hace trampa.//
+    
+    
+ let jugador1 = prompt("ingresa: PIEDRA, PAPEL, o TIJERA ");
+let jugador2 = prompt("ingresa: PIEDRA, PAPEL, o TIJERA ");
+
+if(jugador1.toLowerCase() ==jugador2.toLowerCase()){
+    console.log(" jugadores empatados");
+}
+else if (
+    (jugador1.toLowerCase() == "piedra" && jugador2.toLowerCase() == "tijera") ||
+    (jugador1.toLowerCase() == "tijera" && jugador2.toLowerCase() == "papel") ||
+    (jugador1.toLowerCase() == "papel" && jugador2.toLowerCase() == "piedra")
+    ) {
+    console.log(`Gana jugador 1: ${jugador1} le gana a ${jugador2}`);
+}
+else if(
+    (jugador2.toLowerCase()== "piedra" && jugador1.toLowerCase() =="tijera") ||
+    (jugador2.toLowerCase() == "papel" && jugador1.toLowerCase() == "piedra") ||
+    (jugador2.toLowerCase() == "tijera" && jugador1.toLowerCase() == "papel")
+    ){
+        console.log(`gana jugador 2: ${jugador2} le gana a ${jugador1}`);
+    }
+
+    /*11Realizar un programa que permita el ingreso de un color y utilizando “switch” mostrar por pantalla los siguientes mensajes según las opciones:
+    ● Blanco o Negro: Falta de color.
+    ● Verde: El color de la naturaleza.
+    ● Azul: El color del agua.
+    ● Amarillo: El color del sol.
+    ● Rojo: El color del fuego.
+    ● Marrón: el color de la tierra.
+    ● y para cualquier otro valor: Excelente elección, no lo teníamos pensado.*/
+
+
+     let colorUsuario = prompt("ingresa un color");
+    switch
+     (colorUsuario.toLowerCase()) {
+        case "blanco" :  "negro"
+            console.log("Falta de color.")
+            break;
+
+        case "negro":
+            console.log("falta de color")
+        break;
+
+        case "verde":
+            console.log("el color de la naturaleza")
+            break;
+
+        case "azul":
+            console.log("el color del agua")
+        break;
+
+        case "amarillo":
+            console.log("el color del sol")
+        break;
+
+        case "rojo":
+            console.log("el color del juego")
+            break;
+        
+        case " marorn":
+            console.log("el color de la naturaleza")
+        
+        break;
+
+        default:
+                console.log(" Excelente elección, no lo teníamos pensado.")
+            break;
+    }
+
+    /*12.Realizar un programa que permita el ingreso de 2 valores numéricos y una operación. 
+    Según sea la operación ingresada (suma, resta, multiplicación, división) el programa deberá mostrar en pantalla un mensaje junto con el resultado.
+     En caso de haber elegido división realizar la operación siempre que sea posible o mostrar un mensaje de ERROR si el divisor ingresado fue 0.*/
+
+    let num1 =Number(prompt("ingresa el primer  valor  numerico"));
+     let num2 =Number(prompt("ingresa el segundo valor numerico"));
+     let operacion = prompt("coloca tu operacion de preferencia : +,-,* ,%");
+
+     if(operacion == "+"){
+        let suma = num1 + num2
+        console.log(suma)
+     }
+     else if(operacion == "-" ){
+        let resta = num1 - num2
+        console.log(resta)
+     }
+     else if(operacion == "*"){
+        let multiplicación = num1 * num2
+        console.log(multiplicación)
+     }
+     else if (operacion=="%" && num2 != 0){
+        let division = num1 /num2
+        console.log(division)
+     }
+     else{
+        console.log("ERROR");
+     }
+
+    /* 13 Crear un programa que permita ingresar todos los datos de tu documento nacional de identidad,
+    mostrar por pantalla un mensaje que imprima todos los datos ingresados y pregunte si están correctos los mismos. 
+    En caso afirmativo, crear un objeto llamado dni con todos los datos ingresados y mostrarlos por consola con console.table() más un mensaje de registro exitoso,
+    en caso de que la persona rechace confirmar los datos, mostrar un mensaje que diga: vuelva a intentarlo en 1 mes.*/
+
+let documentoUsuario = Number(prompt("indica tu DNI abajo, por favor"));
+     console.log(documentoUsuario)
+     let preguntaUusario= prompt("los datos ingresados son correctos? SI|| NO")
+     if(preguntaUusario.toLowerCase() == "si"){
+        let dni ={
+            documentoUsuario 
+        };
+        console.table(dni.documentoUsuario + "registro exitoso");
+     }
+     else{
+        console.log("vuelva a intentarlo en 1 mes");
+     }
+
+
+
+
+    
